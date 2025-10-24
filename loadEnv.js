bdabb58d8db911e9cf6c44be4a3c7e2f37c3e694
Nhan Le <johnnhan85@gmail.com>
2025-10-25 01:22:27 +0700

    fix

--- loadEnv.js
+++ loadEnv.js
@@ -1,7 +1,11 @@
 import fs from 'fs'
-      
-      const envConfig = JSON.parse(fs.readFileSync('env.json', 'utf8'));
-      
-      Object.keys(envConfig).forEach(key => {
-        process.env[key] = envConfig[key];
-      });
+
+try {
+  const envConfig = JSON.parse(fs.readFileSync('env.json', 'utf8'));
+  Object.keys(envConfig).forEach(key => {
+    process.env[key] = envConfig[key];
+  });
+} catch (error) {
+  // env.json not found, use environment variables from platform
+  console.log('env.json not found, using platform environment variables');
+}
